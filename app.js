582d443e618494490fd1241fc8fb927ff4e666eb
Pratham Agrawal <agrawalpratham2706@gmail.com>
2026-06-16 11:45:32 +0530

    done with delete and read user

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
 const path=require('path');
+const userModel=require('./models/user');
 
 app.set('view engine','ejs');
 app.use(express.json());
@@ -10,11 +11,24 @@ app.use(express.static(path.join(__dirname,'public')));
 app.get('/',(req,res)=>{
     res.render('index');
 })
-app.get('/read',(req,res)=>{
-    res.render('read');
+app.get('/read',async (req,res)=>{
+    let readUsers= await userModel.find();
+
+    res.render('read',{useers:readUsers});
 })
-app.post('/create',(req,res)=>{
-    
+app.get('/delete/:id',async (req,res)=>{
+    let deleteUser= await userModel.findOneAndDelete({_id:req.params.id})
+    res.redirect('/read')
+})
+app.post('/create',async (req,res)=>{
+    let{name,email,image}=req.body;
+    let createdUser= await userModel.create({
+        name,
+        email,
+        image
+    })
+    res.redirect('/read');
+
 })
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
